8eb4492fc042474c5189ba4955e20ed1882e8098
katia-otot <katiagadea@gmail.com>
2025-01-06 16:11:22 -0300

    estructura del inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
+  standalone: true,
+  styleUrl: './app.component.css',
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `
+ 
 })
 export class AppComponent {
   title = 'cmino-tierra';
